bd27d1d80d0c2fa3dd46be4f6e09c6182be9f468
Prajakta <prajaktadesai@hotmail.com>
2016-05-12 17:21:30 +0530

    Added code for diamond with name problem

---
+++ src/com/company/DiamondWithName.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DiamondWithName {
+    public static int n;
+    public static void printStar(int spaces, int stars){
+        if(stars==n*2-1)
+            return;
+        for (int i = 0; i <spaces ; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i <stars ; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        printStar(spaces-1, stars+2);
+    }
+
+    public static void printStarsREverse(int spaces, int stars){
+        if(stars<1)
+            return;
+        for (int i = 0; i <spaces ; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i <stars ; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        printStarsREverse(spaces+1, stars-2);
+    }
+    public static void main(String [] args){
+        Scanner scanner=new Scanner(System.in);
+        n = scanner.nextInt();
+        printStar(n-1,1);
+        System.out.println("prajakta");
+        printStarsREverse(1,n*2-3);
+    }
+}
