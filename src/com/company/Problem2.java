2eeaad3fa0d70e95650b83af39646710942da465
Prajakta <prajaktadesai@hotmail.com>
2016-05-12 16:22:29 +0530

    Added code for problem of drawing a horizontal line

---
+++ src/com/company/Problem2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String [] args){
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        for (int i = 0; i <n ; i++) {
+            System.out.print("*");
+        }
+
+    }
+}
